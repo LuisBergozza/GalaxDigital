bf41d4b93828d43e89f99127aeee2b807ea80d23
Luís Bergozza <augustoluis22525@gmail.com>
2026-09-07 23:49:07 -0300

    a

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,7 +50,7 @@ export const Navbar: React.FC<NavbarProps> = ({ isLightMode, onToggleTheme }) =>
           {/* Logo */}
           <a href="#" className="flex items-center group z-50">
             <img
-              src={isLightMode ? '/logo preta.png' : '/logo_navbar.webp'}
+              src={isLightMode ? '/logo preta.png' : '/logo-navbar-320.webp'}
               alt="Galax Digital"
               width={160}
               height={56}
